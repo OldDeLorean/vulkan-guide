7b5830db611edc39b0bb9749ec84617fc66a8b0b
Fedor Rodionov <fedor.rodionov@kaust.edu.sa>
2024-12-02 23:09:03 +0300

    chapter 2

--- src/vk_images.h
+++ src/vk_images.h
@@ -1,7 +1,13 @@
+#pragma once
 
-#pragma once 
+#include <vulkan/vulkan.h>
 
 namespace vkutil {
 
+void transition_image(VkCommandBuffer cmd, VkImage image, VkImageLayout currentLayout, VkImageLayout newLayout);
 
-};
+void copy_image_to_image(VkCommandBuffer cmd, VkImage source, VkImage destination, VkExtent2D srcSize,
+                         VkExtent2D dstSize);
+
+bool load_shader_module(const char* filePath, VkDevice device, VkShaderModule* outShaderModule);
+}  // namespace vkutil
